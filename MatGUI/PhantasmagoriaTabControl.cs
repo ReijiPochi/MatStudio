01906f79455133a642cdbdc0af6e497122bed4b1
ReijiPochi <koyuki-2009@live.jp>
2016-08-11 22:47:43 +0900

    8/11-2

---
+++ MatGUI/PhantasmagoriaTabControl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatGUI
+{
+    public class PhantasmagoriaTabControl : TabControl
+    {
+        static PhantasmagoriaTabControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PhantasmagoriaTabControl), new FrameworkPropertyMetadata(typeof(PhantasmagoriaTabControl)));
+        }
+
+        protected override void OnSelectionChanged(SelectionChangedEventArgs e)
+        {
+            base.OnSelectionChanged(e);
+        }
+
+
+        /// <summary>
+        /// i1 と i2 の場所を入れ替えます
+        /// </summary>
+        /// <param name="i1"></param>
+        /// <param name="i2"></param>
+        public void ReplaceItems(PhantasmagoriaTabItem i1, PhantasmagoriaTabItem i2)
+        {
+            int index_ti1 = Items.IndexOf(i1);
+            int index_ti2 = Items.IndexOf(i2);
+
+            Items.Remove(i1);
+            Items.Remove(i2);
+
+            if (index_ti1 > index_ti2)
+            {
+                Items.Insert(index_ti1 - 1, i2);
+                Items.Insert(index_ti2, i1);
+            }
+            else
+            {
+                Items.Insert(index_ti1, i2);
+                Items.Insert(index_ti2, i1);
+            }
+        }
+    }
+}
---
+++ MatGUI/PhantasmagoriaTabItem.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatGUI
+{
+    public class PhantasmagoriaTabItem : TabItem
+    {
+        static PhantasmagoriaTabItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PhantasmagoriaTabItem), new FrameworkPropertyMetadata(typeof(PhantasmagoriaTabItem)));
+        }
+
+        public PhantasmagoriaTabItem()
+        {
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            MaskRect = GetTemplateChild("MatMaskRect") as Rectangle;
+            MaskRect.MouseLeave += MatPhantasmagoriaTabItem_MouseLeave;
+            MaskRect.DragEnter += MatPhantasmagoriaTabItem_DragEnter;
+        }
+
+        public bool IsActivePanel
+        {
+            get { return (bool)GetValue(IsActivePanelProperty); }
+            set { SetValue(IsActivePanelProperty, value); }
+        }
+        public static readonly DependencyProperty IsActivePanelProperty =
+            DependencyProperty.Register("IsActivePanel", typeof(bool), typeof(PhantasmagoriaTabItem), new PropertyMetadata(false));
+
+        /// <summary>
+        /// ドラッグ、ドロップ用のマスク
+        /// </summary>
+        protected Rectangle MaskRect;
+
+
+        private void MatPhantasmagoriaTabItem_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragDrop.DoDragDrop(this, this, DragDropEffects.Move);
+                IsSelected = true;
+            }
+        }
+
+        private void MatPhantasmagoriaTabItem_DragEnter(object sender, DragEventArgs e)
+        {
+            PhantasmagoriaTabItem source = e.Data.GetData(typeof(PhantasmagoriaTabItem)) as PhantasmagoriaTabItem;
+
+            if (source != null && source != this)
+            {
+                if(source.Parent == Parent)
+                {
+                    PhantasmagoriaTabControl p = Parent as PhantasmagoriaTabControl;
+                    if (p == null) throw new Exception("MatPhantasmagoriaTabItem のParentが MatPhantasmagoriaTabControl でありません");
+
+                    p.ReplaceItems(this, source);
+                    source.IsSelected = true;
+                }
+            }
+        }
+
+    }
+}
