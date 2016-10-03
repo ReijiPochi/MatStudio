749cc1c7d358361f04b29ec81aff4865df131171
ReijiPochi <koyuki-2009@live.jp>
2016-10-03 14:46:37 +0900

    10/3-2

--- MatGUI/MatGraph.cs
+++ MatGUI/MatGraph.cs
@@ -12,10 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.ComponentModel;
+using System.Windows.Forms.Integration;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
-
+using OpenTK.Graphics;
 
 namespace MatGUI
 {
@@ -26,11 +27,67 @@ namespace MatGUI
             DefaultStyleKeyProperty.OverrideMetadata(typeof(MatGraph), new FrameworkPropertyMetadata(typeof(MatGraph)));
         }
 
+        public MatGraph()
+        {
+            if (!DesignerProperties.GetIsInDesignMode(this))
+            {
+                glc = new GLControl(new GraphicsMode(GraphicsMode.Default.AccumulatorFormat, 24, 0, 0));
+                glc.Load += Glc_Load;
+                glc.Paint += Glc_Paint;
+            }
+        }
+
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
 
-            
+            WindowsFormsHost host = GetTemplateChild("Host") as WindowsFormsHost;
+            host.Child = glc;
+            host.SizeChanged += Host_SizeChanged;
         }
+
+        private GLControl glc;
+
+        [Description("ドローイングエリアの背景色"), Category("IBFramework")]
+        public Color BackgroundColor
+        {
+            get { return (Color)GetValue(BackgroundColorProperty); }
+            set { SetValue(BackgroundColorProperty, value); }
+        }
+        public static readonly DependencyProperty BackgroundColorProperty =
+            DependencyProperty.Register("BackgroundColor", typeof(Color), typeof(MatGraph), new PropertyMetadata(Color.FromArgb(255, 50, 50, 50), new PropertyChangedCallback(OnBackgroundColorChanged)));
+
+        private static void OnBackgroundColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            MatGraph sender = d as MatGraph;
+            if (sender != null)
+            {
+                sender.glc.MakeCurrent();
+                GL.ClearColor(sender.BackgroundColor.R / 255.0f, sender.BackgroundColor.G / 255.0f, sender.BackgroundColor.B / 255.0f, 1.0f);
+                sender.glc.Refresh();
+            }
+        }
+
+
+        private void Host_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            glc.Width = (int)ActualWidth;
+            glc.Height = (int)ActualHeight;
+        }
+
+        private void Glc_Load(object sender, EventArgs e)
+        {
+            glc.MakeCurrent();
+        }
+
+        private void Glc_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            glc.MakeCurrent();
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            glc.SwapBuffers();
+        }
+
     }
 }
